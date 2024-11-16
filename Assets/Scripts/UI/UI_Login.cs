0bfded7a15406ba79e540efbc0b7f78ce3e64ae3
dhghgh189 <dhghgh189@gmail.com>
2024-11-16 23:58:08 +0900

    [Feat] 이메일 인증 확인, 닉네임 설정 및 로그인 기능 구현
    - 로그인 시 이메일 인증이 안된 경우 인증 요청 메일 송신후 대기
    - 로그인 시 닉네임 설정이 안된 경우 닉네임 설정 진행
    - 이메일 인증과 닉네임 설정이 완료되면 로비에 로그인 가능

--- Assets/Scripts/UI/UI_Login.cs
+++ Assets/Scripts/UI/UI_Login.cs
@@ -31,8 +31,10 @@ public class UI_Login : UIBase
         AddUIEvent(Get("btnSignUp"), Enums.UIEvent.PointerClick, ShowSignUpPanel);
         // Check Email 버튼에 이벤트 추가
         AddUIEvent(Get("btnCheckEmail"), Enums.UIEvent.PointerClick, CheckEmail);
-        // Confirm 버튼에 이벤트 추가
+        // Sing up Confirm 버튼에 이벤트 추가
         AddUIEvent(Get("btnConfirm"), Enums.UIEvent.PointerClick, ConfirmSignUp);
+        // NickName Confirm 버튼에 이벤트 추가
+        AddUIEvent(Get("btnConfirmNickName"), Enums.UIEvent.PointerClick, ConfirmNickName);
 
         // signUpIdInputField 이벤트 추가
         Get<InputField>("signUpIdInputField").onValueChanged.AddListener(str => 
@@ -51,21 +53,147 @@ public class UI_Login : UIBase
     public void Login(PointerEventData eventData)
     {
         // 입력값을 받아온다.
-        string name = Get<InputField>("idInputField").text;
+        string email = Get<InputField>("idInputField").text;
+        string pass = Get<InputField>("passInputField").text;
 
         // 유효한지 검사
-        if (string.IsNullOrEmpty(name))
+        if (string.IsNullOrEmpty(email))
         {
             Debug.LogWarning("Please Enter E-mail!");
             ShowInfoPopup("Please Enter E-mail!");
             return;
         }
 
+        if (string.IsNullOrEmpty(pass))
+        {
+            Debug.LogWarning("Please Enter Password!");
+            ShowInfoPopup("Please Enter Password!");
+            return;
+        }
+
         ShowInfoPanel("Please Wait...", Color.black);
 
-        // 닉네임 설정 후 연결시도
-        PhotonNetwork.NickName = name;
+        // 로그인 요청
+        BackendManager.Auth.SignInWithEmailAndPasswordAsync(email, pass)
+            .ContinueWithOnMainThread(task =>
+        {
+            if (task.IsCanceled)
+            {
+                HideInfoPanel();
+                ShowInfoPopup("SignInWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            // 로그인 실패 시
+            if (task.IsFaulted)
+            {
+                HideInfoPanel();
+                Debug.Log("SignInWithEmailAndPasswordAsync encountered an error.");
+
+                // Exception을 FirebaseException으로 변환하여 ErrorCode를 얻어오고
+                // AuthError 열거형으로 변환하여 로그인 실패 사유를 확인
+                AuthError eError = (AuthError)(task.Exception.GetBaseException() as Firebase.FirebaseException).ErrorCode;
+
+                // 에러를 통한 팝업 처리
+                CheckAuthError(eError);
+                return;
+            }
+
+            // 로그인 성공 시
+            Debug.Log("Login Successfully!");
+
+            // 로그인한 유저를 가져온다.
+            FirebaseUser user = task.Result.User;
+
+            // 아직 이메일 인증을 하지 않은 경우
+            if (!user.IsEmailVerified)
+            {
+                HideInfoPanel();
+                // 이메일 인증을 위한 패널 표시
+                Get("VerifyEmailPanel").SetActive(true);
+            }
+            // 아직 닉네임을 설정하지 않은 경우
+            else if (string.IsNullOrEmpty(user.DisplayName))
+            {
+                HideInfoPanel();
+                // 닉네임 설정을 위한 패널 표시
+                Get("NickNamePanel").SetActive(true);
+            }
+            // 이메일 인증 및 닉네임 설정이 완료된 유저의 경우
+            else
+            {
+                // 서버 연결 시도
+                ConnectToMasterServer(user);
+            }
+        });
+    }
+
+    public void ConfirmNickName(PointerEventData eventData)
+    {
+        string nickName = Get<InputField>("NickNameInputField").text;
+        if (string.IsNullOrEmpty(nickName))
+        {
+            ShowInfoPopup("Please Enter NickName!", Color.red);
+            return;
+        }
+
+        FirebaseUser user = BackendManager.Auth.CurrentUser;
+
+        // 유저 프로필 설정을 위한 UserProfile 객체
+        UserProfile profile = new UserProfile();
+        // nickName필드 설정
+        profile.DisplayName = nickName;
+        // 프로필 갱신 요청
+        user.UpdateUserProfileAsync(profile).ContinueWithOnMainThread(task =>
+        {
+            // 작업이 취소된 경우
+            if (task.IsCanceled)
+            {
+                ShowInfoPopup("UpdateUserProfileAsync was canceled.");
+                return;
+            }
+            // 작업이 완료되지 않은 경우
+            if (task.IsFaulted)
+            {
+                Debug.LogError($"UpdateUserProfileAsync encountered an error");
+                string msg = task.Exception.Message;
+
+                // 완료되지 못한 사유 출력
+                ShowInfoPopup($"{msg.Substring(msg.IndexOf('(') + 1).Replace(')', ' ')}");
+                return;
+            }
+
+            Debug.Log($"Set NickName Successfully! : {nickName}");
+
+            // 마스터 서버 연결 요청
+            ConnectToMasterServer(user);
+        });
+    }
+
+    public void ConnectToMasterServer(FirebaseUser user)
+    {
+        // 닉네임 지정 및 연결
+        PhotonNetwork.NickName = user.DisplayName;
         PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void CheckAuthError(AuthError eError)
+    {
+        switch (eError)
+        {
+            // 존재하지 않는 이메일인 경우 별도 메시지 출력
+            // Wrong Password로 인한 실패 사실을 알리는 경우 해킹의 위험이 있으므로
+            // Wrong Password case는 제외하였음
+            case AuthError.UserNotFound:
+                ShowInfoPopup("User not exist!", Color.red);
+                break;
+            case AuthError.Failure:
+            case AuthError.ApiNotAvailable:
+                ShowInfoPopup("System Error!", Color.red);
+                break;
+            default:
+                ShowInfoPopup("Check Your Account!", Color.red);
+                break;
+        }
     }
 
     public void ShowInfoPanel(string msg, Color color, bool bShow = true)
@@ -200,7 +328,7 @@ public class UI_Login : UIBase
             // 작업이 완료되지 않은 경우
             if (task.IsFaulted)
             {
-                Debug.LogError($"CreateUserWithEmailAndPasswordAsync encountered an error");
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error");
                 string msg = task.Exception.Message;
 
                 // 완료되지 못한 사유 출력
